e7bf1a23287dd43e0cf46310cfee729272f665cd
Cameron Graham <cameron.g882@gmail.com>
2020-06-17 22:55:19 -0400

    added a score element and functions to increase score when the choice is right and decrease time when choice is wrong  and the end game when we are out of questions

--- assets/style/app.js
+++ assets/style/app.js
@@ -8,7 +8,17 @@ var questions = [
         title: "Example Question 2:",
         choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
         answer: "Choice 1"
-    }
+    },
+    {
+        title: "Example Question 3:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 1"
+    },
+    {
+        title: "Example Question 4:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 4"
+    },
 ];
 
 var questionBoxEl = document.querySelector("#question-box");
@@ -23,9 +33,12 @@ var questionText = document.createElement("p");
 
 var highScores = document.querySelector("#highscores");
 
+var scoreEl = document.querySelector("#score");
+
 // create clobal variables and a variable to hold our index
 var timer = 60;
 var qindex = 0;
+var score = 0;
 
 // create a function that loads a welcome page and a start button to begin the quiz
 function welcomePage() {
@@ -49,12 +62,14 @@ function startQuiz() {
 function showTimer() {
     timer = 60;
     timerEl.textContent = "Time Left: " + timer;
+    scoreEl.textContent = "Score: " + score;
     // create setInterval and store it to a variable
     var timerInterval = setInterval(function () {
         timer--;
         timerEl.textContent = "Time Left: " + timer;
         if (timer === 0) {
             clearInterval(timerInterval);
+            endGame();
         }
 
     }, 1000)
@@ -78,6 +93,7 @@ function nextQuestion() {
         var answerBtn = document.createElement("button");
         answerBtn.classList.add("choiceBtn");
         answerBtn.textContent = currentQuestion.choices[index];
+        answerBtn.onclick = checkAnswer;
         answersDiv.appendChild(answerBtn);
 
     }
@@ -85,17 +101,34 @@ function nextQuestion() {
 
 };
 
-function checkAnswer(event) {
-    // if event.target.matches (choiceBtn class)
-    if (event.target.matches(".choiceBtn")) {
-        // Logic to check for right answer
+function checkAnswer() {
+    var userChoice = this.textContent;
+    // Logic to check for right answer
+    if (userChoice === questions[qindex].answer) {
+        // add points for right answer and move to next question
+        score = score + 10;
+        scoreEl.textContent = score;
         qindex++;
-        nextQuestion()
+        // subtract 5 seconds for every wrong answer
+    } else {
+        timer -= 5;
+        timerEl.textContent = timer;
     }
+    // end game once we run out of questions
+    if (qindex === questions.length - 1) {
+        endGame()
+    } else {
+        nextQuestion();
+    }
+}
+
+function endGame() {
+    clearInterval(timerInterval)
+    questionBoxEl.style.display = "none"
+    
 }
 
 welcomePage();
 
 // event listener to start the quiz on button click
 startBtn.addEventListener("click", startQuiz);
-document.addEventListener("click", checkAnswer);
